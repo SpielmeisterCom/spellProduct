1c5aa1f1099a7ef955d6b9486c38c8a8236e89ba
Julian Haupt <jh@spielmeister.com>
2012-09-14 01:52:02 +0200

    refactored platform to make use of the templating system

--- projects/battleblast/library/templates/battleblast/system/lightsPulsator.js
+++ projects/battleblast/library/templates/battleblast/system/lightsPulsator.js
@@ -19,7 +19,7 @@ define(
 
 		var process = function( spell, timeInMs, deltaTimeInMs ) {
 
-			var opacity = Math.abs( Math.sin(timeInMs/300) )
+			var opacity = Math.abs( Math.sin(timeInMs/1500) )
 
 			this.updateLights( timeInMs, opacity )
 		}
