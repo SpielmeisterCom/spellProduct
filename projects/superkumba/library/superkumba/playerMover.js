ebb278cec4bda20c8d829508487862a32ab1163e
Julian Haupt <jh@spielmeister.com>
2012-10-11 01:01:44 +0200

    some experiments for superkumba

---
+++ projects/superkumba/library/superkumba/playerMover.js
@@ -0,0 +1,35 @@
+define(
+	'superkumba/playerMover',
+	[
+		'spell/functions'
+	],
+	function(
+		_
+	) {
+		"use strict"
+		
+        var cleanUp = function( spell ) {
+            
+        }
+        
+        var init = function( spell ) {
+            
+        }
+        
+        var process = function( spell ) {
+            
+        }
+        
+        var playerMover = function( spell ) {
+
+		}
+
+		playerMover.prototype = {
+			cleanUp : cleanUp,
+			init : init,
+			process : process
+		}
+
+		return playerMover
+	}
+)
