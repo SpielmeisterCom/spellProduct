8eb05d9f0af8dca140618360937c45d234332ff2
Martin Dederer <md@spielmeister.com>
2012-10-17 15:43:52 +0200

    Added box2dTest project

---
+++ projects/box2dTest/library/box2dTest/bodyTest.js
@@ -0,0 +1,128 @@
+/**
+ * @class box2dTest.bodyTest
+ * @singleton
+ */
+
+define(
+	'box2dTest/bodyTest',
+	[
+		'spell/shared/util/input/keyCodes',
+		'spell/math/util',
+
+		'spell/functions'
+	],
+	function(
+		keyCodes,
+		mathUtil,
+
+		_
+	) {
+		'use strict'
+
+
+
+		var positions = 5,
+			positionIndex = 0
+
+		var createSpawnPosition = function( spell ) {
+			var x = ( mathUtil.modulo( positionIndex++, positions ) - Math.floor( positions / 2 ) ) * 7
+
+			return [ x, 30 ]
+		}
+
+
+		/**
+		 * Creates an instance of the system.
+		 *
+		 * @constructor
+		 * @param {Object} [spell] The spell object.
+		 */
+		var BodyTest = function( spell ) {
+			this.inputEvents = spell.inputEvents
+		}
+
+		BodyTest.prototype = {
+			/**
+		 	 * Gets called when the system is created.
+		 	 *
+		 	 * @param {Object} [spell] The spell object.
+			 */
+			init: function( spell ) {
+
+			},
+
+			/**
+		 	 * Gets called when the system is destroyed.
+		 	 *
+		 	 * @param {Object} [spell] The spell object.
+			 */
+			destroy: function( spell ) {
+
+			},
+
+			/**
+		 	 * Gets called when the system is activated.
+		 	 *
+		 	 * @param {Object} [spell] The spell object.
+			 */
+			activate: function( spell ) {
+
+			},
+
+			/**
+		 	 * Gets called when the system is deactivated.
+		 	 *
+		 	 * @param {Object} [spell] The spell object.
+			 */
+			deactivate: function( spell ) {
+
+			},
+
+			/**
+		 	 * Gets called to trigger the processing of game state.
+		 	 *
+			 * @param {Object} [spell] The spell object.
+			 * @param {Object} [timeInMs] The current time in ms.
+			 * @param {Object} [deltaTimeInMs] The elapsed time in ms.
+			 */
+			process: function( spell, timeInMs, deltaTimeInMs ) {
+				if( this.inputEvents.length === 0 ) return
+
+				var event = this.inputEvents[ 0 ]
+
+				if( event.type === 'keydown' &&
+					event.keyCode === keyCodes[ 'RIGHT_ARROW' ] ) {
+
+					var components = spell.EntityManager.getComponentDictionaryById( 'spell.component.box2d.simpleBox' )
+
+					if( _.size( components ) === 0 ) return
+
+					var id = _.find(
+						_.keys( components ),
+						function( id ) {
+							return id.charAt( 0 ) !== '_'
+						}
+					)
+
+					if( !id ) return
+
+					spell.EntityManager.removeEntity( id )
+
+				} else if( event.type === 'keydown' &&
+					event.keyCode === keyCodes[ 'LEFT_ARROW' ] ) {
+
+					spell.EntityManager.createEntity( {
+						entityTemplateId : 'box2dTest.smallCrate',
+						config : {
+							"spell.component.2d.transform" : {
+								"translation" : createSpawnPosition()
+							}
+						}
+					} )
+				}
+			}
+		}
+
+		return BodyTest
+	}
+)
---
+++ projects/box2dTest/library/spell/system/keyFrameAnimation.js
@@ -0,0 +1,165 @@
+define(
+	'spell/system/keyFrameAnimation',
+	[
+		'spell/shared/Easing',
+		'spell/math/util',
+		'spell/math/vec2',
+
+		'spell/functions'
+	],
+	function(
+		Easing,
+		mathUtil,
+		vec2,
+
+		_
+	) {
+		'use strict'
+
+
+		var clamp        = mathUtil.clamp,
+			isInInterval = mathUtil.isInInterval,
+			modulo       = mathUtil.modulo
+
+		var getKeyFrameIdA = function( keyFrames, offset ) {
+			for( var i = 0, numKeyFrames = keyFrames.length; i < numKeyFrames; i++ ) {
+				if( keyFrames[ i ].time > offset ) {
+					return i - 1
+				}
+			}
+		}
+
+		var getEasingFunction = function( name ) {
+			if( !name ) name = 'Linear'
+
+			var fn = Easing[ name ]
+
+			if( !fn ) {
+				throw 'Error: Unkown easing function \'' + name + '\'.'
+			}
+
+			return fn
+		}
+
+		var createOffset = function( deltaTimeInMs, animationLengthInMs, offsetInMs, replaySpeed, reversed ) {
+			return Math.round( offsetInMs + deltaTimeInMs * replaySpeed * ( reversed ? -1 : 1 ) )
+		}
+
+		var normalizeOffset = function( animationLengthInMs, offsetInMs, looped ) {
+			return looped ?
+				modulo( offsetInMs, animationLengthInMs ) :
+				clamp( offsetInMs, 0, animationLengthInMs )
+		}
+
+		var updatePlaying = function( animationLengthInMs, offsetInMs, looped ) {
+			return looped ?
+				true :
+				isInInterval( offsetInMs, 0, animationLengthInMs )
+		}
+
+		var setValue = function( component, attributeId, value ) {
+			var attribute = component[ attributeId ]
+
+			if( _.isArray( attribute ) ) {
+				vec2.set( value, attribute )
+
+			} else {
+				component[ attributeId ] = value
+			}
+		}
+
+		var lerp = function( a, b, t ) {
+			return a + ( b - a ) * t
+		}
+
+		var init = function( spell ) {}
+
+		var process = function( spell, timeInMs, deltaTimeInMs ) {
+			var entityManager      = this.entityManager,
+				keyFrameAnimations = this.keyFrameAnimations
+
+			for( var id in keyFrameAnimations ) {
+				var keyFrameAnimation      = keyFrameAnimations[ id ],
+					keyFrameAnimationAsset = keyFrameAnimation.asset,
+					animate                = keyFrameAnimationAsset.animate,
+					lengthInMs             = keyFrameAnimationAsset.length
+
+				if( !keyFrameAnimation.playing ) continue
+
+				var rawOffset = createOffset(
+					deltaTimeInMs,
+					lengthInMs,
+					keyFrameAnimation.offset,
+					keyFrameAnimation.replaySpeed,
+					keyFrameAnimation.reversed
+				)
+
+				var offset = normalizeOffset( lengthInMs, rawOffset, keyFrameAnimation.looped )
+				keyFrameAnimation.offset  = offset
+				keyFrameAnimation.playing = updatePlaying( lengthInMs, rawOffset, keyFrameAnimation.looped )
+
+
+				for( var componentId in animate ) {
+					var componentAnimation = animate[ componentId ],
+						component          = entityManager.getComponentById( componentId, id )
+
+					if( !component ) {
+						throw 'Error: Unable to access component \'' + componentId + '\' of entity \'' + id + '\'.'
+					}
+
+					for( var attributeId in componentAnimation ) {
+						var attributeAnimation = componentAnimation[ attributeId ],
+							keyFrames          = attributeAnimation.keyFrames,
+							animationLength    = attributeAnimation.length,
+							keyFrameIdA        = getKeyFrameIdA( keyFrames, offset )
+
+						if( keyFrameIdA < 0 ) {
+							// set to first key frame value
+							setValue( component, attributeId, keyFrames[ 0 ].value )
+
+							continue
+						}
+
+						if( keyFrameIdA === undefined ) {
+							// set to last key frame value
+							setValue( component, attributeId, keyFrames[ keyFrames.length - 1 ].value )
+
+							continue
+						}
+
+						var keyFrameIdB = keyFrameIdA + 1,
+							keyFrameA   = keyFrames[ keyFrameIdA ],
+							keyFrameB   = keyFrames[ keyFrameIdB ]
+
+						// interpolate between key frame A and B
+						var attribute       = component[ attributeId ],
+							attributeOffset = offset - keyFrameA.time,
+							easingFunction  = getEasingFunction( keyFrameB.interpolation ),
+							t               = easingFunction( attributeOffset / ( keyFrameB.time - keyFrameA.time ) )
+
+						if( _.isArray( attribute ) ) {
+							vec2.lerp( keyFrameA.value, keyFrameB.value, t, attribute )
+
+						} else {
+							component[ attributeId ] = lerp( keyFrameA.value, keyFrameB.value, t )
+						}
+					}
+				}
+			}
+		}
+
+		var KeyFrameAnimation = function( spell ) {
+			this.entityManager = spell.EntityManager
+		}
+
+		KeyFrameAnimation.prototype = {
+			init : init,
+			destroy : function() {},
+			activate : function() {},
+			deactivate : function() {},
+			process : process
+		}
+
+		return KeyFrameAnimation
+	}
+)
